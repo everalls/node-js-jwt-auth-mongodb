d68da4b2d49dcdf9b6037c1d17cde5fd2ed01553
Erika Grin <evgeny.grin@iprosis.com>
2020-10-04 09:32:57 +0300

    updates

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-var corsOptions = {
-	origin: 'http://localhost:8081'
+let corsOptions = {
+	origin: 'http://localhost:8081',
 };
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
+// parse requests of content-type = application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// parse requests of content-type -application/x-www-form-urlencoed
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
+//simple route
 app.get('/', (req, res) => {
-	res.json({
-		mesage: 'Welcome to bezcoder application'
-	});
+	res.json({ message: 'hey from my application' });
 });
 
-//set port, listen to requests
+// set por, listen to requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-	console.log(`Sever is running on port ${PORT}`);
+	console.log('Server is running on port', PORT);
 });
