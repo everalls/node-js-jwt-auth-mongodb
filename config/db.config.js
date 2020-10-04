fc226fbbb585ca958da30ea7967c7a96cf583f0e
Erika Grin <evgeny.grin@iprosis.com>
2020-10-04 18:39:45 +0300

    continue with tutorial

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	url:
+		'mongodb+srv://everall:ksvu4377@everall-eytlp.gcp.mongodb.net/easy-notes?retryWrites=true&w=majority',
+};
+// to local database:
+// run in the terminal (macOS): brew services start mongodb-community@4.2
+//url: 'mongodb://localhost:27017/easy-notess'
